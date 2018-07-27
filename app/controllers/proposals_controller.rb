2cd869ae635d37c5f28704ed210175e8cfcc3db6
DGMarkel <davidgmarkel@gmail.com>
2018-07-27 12:12:43 -0400

    basic proposal show page, fix issues with proposals form, add proposals #new, #create

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -8,4 +8,23 @@ class ProposalsController < ApplicationController
     @proposal = Proposal.find_by(id: params[:id])
   end
 
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    if @proposal.save
+      redirect_to proposal_path(@proposal)
+    else
+      render new_proposal_path
+    end
+  end
+
+  private
+
+    def proposal_params
+      params.require(:proposal).permit(:name, :description, :pitch, :image_url)
+    end
+
 end
