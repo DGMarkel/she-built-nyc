05a601e61e9383408d28e5ac84f00a923134ef5c
DGMarkel <davidgmarkel@gmail.com>
2018-07-27 12:26:17 -0400

    add gems pry, bcrypt, add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_one :proposal
   validates :name, uniqueness: true #um....how am I going to deal with multiple users with the same name?
+  has_secure_password
 end
