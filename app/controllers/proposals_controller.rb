699fab95ee98d92a30ddfad630d49ccb727c7528
DGMarkel <davidgmarkel@gmail.com>
2018-07-27 11:11:46 -0400

    add #index, #show to User and Proposal controllers

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,2 +1,11 @@
 class ProposalsController < ApplicationController
+
+  def index
+    @proposals = Proposal.all
+  end
+
+  def show
+    @proposal = Proposal.find_by(id: params[:id])
+  end
+
 end
