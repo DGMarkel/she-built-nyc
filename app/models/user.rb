8c78193a163798ed51f6891b6bef380be3516279
DGMarkel <davidgmarkel@gmail.com>
2018-07-28 00:08:41 -0400

    add user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
   validates :name, uniqueness: true #um....how am I going to deal with multiple users with the same name?
   #need to add attribute validations
   has_secure_password
+
+  def created_today?
+    Date.today.day == self.created_at.day
+  end
+
+  def self.created_today
+    all.select{|user| user if user.created_today?}
+  end
+
 end
