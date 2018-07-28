d72bcf1bed50bef4b3ebbfbf994a72f0925dd606
DGMarkel <davidgmarkel@gmail.com>
2018-07-27 21:04:03 -0400

    join table working, displays ranking on proposal show page, images styled"

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,6 +3,20 @@ class Proposal < ApplicationRecord
   has_many :users, through: :rankings
   validates :name, uniqueness: true
 
+  def count
+    self.rankings.count
+  end
+
+  def rankings_sum
+    sum = 0
+    self.rankings.each {|ranking| sum += ranking.ranking}
+    sum
+  end
+
+  def rankings_average
+    rankings_sum/count
+  end
+
   def self.top_three_proposals
   end
 
