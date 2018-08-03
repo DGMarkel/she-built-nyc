3d600e1bc2097d8724b20bad1e9ad2cf5e26378e
DGMarkel <davidgmarkel@gmail.com>
2018-08-03 19:35:16 -0400

    work towards winning_proposal method

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -23,6 +23,12 @@ class Proposal < ApplicationRecord
     sum
   end
 
+  def number_of_votes
+    sum = 0
+    self.rankings.each {|ranking| sum += 1}
+    sum
+  end
+
   def rankings_average
     if rankings_sum != 0
       rankings_sum/count
@@ -33,6 +39,27 @@ class Proposal < ApplicationRecord
     hash = {}
     all.each {|proposal| hash[proposal] = proposal.rankings_average if proposal.rankings_average && proposal.rankings.count > 1 }
     hash = hash.sort_by {|k,v| v}.reverse[0..2]
+  end
+
+  def self.highest_ranking_average
+    a = all.to_a
+    a.max  {|a,b| a.rankings_average <=> b.rankings_average }
+  end
+
+  def self.highest_number_of_votes
+    a = all.to_a
+    a.max {|a,b| a.number_of_votes <=> b.number_of_votes }
+  end
+
+  def self.highest_possible_ranking
+    highest_ranking_average.voters.count
+  end
+
+  def winning_proposal
+    if highest_ranking == highest_number_of_votes
+
+    # if two proposals have the same number of votes
+
   end
 
 end
