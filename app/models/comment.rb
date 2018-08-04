927c9c563c7d6617da7fe74cddb11fe279a4601f
DGMarkel <davidgmarkel@gmail.com>
2018-08-03 21:06:24 -0400

    add super simple styling to admin show page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,13 @@ class Comment < ApplicationRecord
   scope :oldest_first, -> { order(created_at: :asc)}
   scope :newest_first, -> { order(created_at: :desc)}
 
+
+  def created_today?
+    Date.today.day == self.created_at.day
+  end
+
+  def self.created_today
+    all.select{|proposal| proposal if proposal.created_today?}
+  end
+
 end
