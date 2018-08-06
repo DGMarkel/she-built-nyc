5a0d3b77b56cbdbe8742b6604c3021fdf5560790
DGMarkel <davidgmarkel@gmail.com>
2018-08-06 13:58:16 -0400

    remove redundant helper

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,11 +1,3 @@
 module ProposalsHelper
 
-  def user_name_linked_or_not(instance)
-    if logged_in?
-       link_to instance.user.name, user_path(instance.user)
-    else
-       @proposal.user.name
-    end
-  end
-
 end
