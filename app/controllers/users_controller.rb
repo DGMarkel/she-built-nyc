5b558f57ea607d1c18ee49a94dbe4223c332ae15
DGMarkel <davidgmarkel@gmail.com>
2018-08-06 12:04:09 -0400

    prevent non-admins from viewing admin page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    if !current_user.admin && @user.admin
+      redirect_to root_path
+      flash[:non_admin_warning] = "Permission denied: restricted to admins only"
+    end
   end
 
   def new
