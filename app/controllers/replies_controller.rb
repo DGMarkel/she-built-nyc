d8ff33fedd0f552f27c3eb2f35ed2acc04fc1668
DGMarkel <davidgmarkel@gmail.com>
2018-08-06 10:03:55 -0400

    add flash[:reply_warning] to reply edit page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -21,12 +21,18 @@ class RepliesController < ApplicationController
 
   def edit
     @reply = Reply.find_by(id: params[:id])
+    @comment = Comment.find_by(id: @reply.comment_id)
+    @proposal = Proposal.find_by(id: @comment.proposal_id)
   end
 
   def update
     @reply = Reply.find_by(id: params[:id])
-    @reply.update(content: params[:reply][:content])
-    redirect_to user_comments_path(@reply.comment.user)
+    if @reply.update(content: params[:reply][:content])
+      redirect_to user_comments_path(@reply.comment.user)
+    else
+      flash[:reply_warning] = "Reply must have content."
+      redirect_to edit_reply_path(@reply)
+    end
   end
 
   def destroy
