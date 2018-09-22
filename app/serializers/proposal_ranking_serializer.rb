b6b004e112a72a8bb75325cffadee74f87205c38
DGMarkel <davidgmarkel@gmail.com>
2018-09-22 12:55:07 -0400

    add ProposalRanking serializer

---
+++ app/serializers/proposal_ranking_serializer.rb
@@ -0,0 +1,3 @@
+class ProposalRankingSerializer < ActiveModel::Serializer
+  attributes :id, :ranking, :user_id
+end
--- app/serializers/proposal_serializer.rb
+++ app/serializers/proposal_serializer.rb
@@ -2,5 +2,5 @@ class ProposalSerializer < ActiveModel::Serializer
   attributes :id, :name, :image_url, :description, :pitch, :created_at, :updated_at
   belongs_to :user
   has_many :comments, serializer: CommentSerializer
-  has_many :rankings
+  has_many :rankings, serializer: ProposalRankingSerializer
 end
