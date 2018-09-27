829bd9a130578f84ee559948a872e1f2eaa82f64
DGMarkel <davidgmarkel@gmail.com>
2018-09-26 20:23:44 -0400

    add rankings#index to render ranking API

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,11 @@
 class RankingsController < ApplicationController
   before_action :site_security
 
+def index
+  @rankings = Ranking.all
+  render json: @rankings
+end
+
   def new
     @ranking = Ranking.new
   end
@@ -11,6 +16,5 @@ class RankingsController < ApplicationController
       flash[:ranking_warning] = "Ranking must fall into a range of 1 - 5"
     end
   end
-
 
 end
