2a0fb6e8b82f4cb688902a978b1c3f6df8ebd81d
DGMarkel <davidgmarkel@gmail.com>
2018-09-30 23:06:25 -0400

    add rudimentary replies template

--- app/serializers/reply_serializer.rb
+++ app/serializers/reply_serializer.rb
@@ -1,4 +1,4 @@
 class ReplySerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :created_at, :updated_at
+  attributes :id, :content, :user_id, :user, :created_at, :updated_at
   belongs_to :user
 end
