9147abb5009d72c4a510b4d87fd4afb1bf49c9d5
DGMarkel <davidgmarkel@gmail.com>
2018-10-24 18:51:45 -0400

    move current user profile functions to current_user_profile.js

---
+++ app/assets/javascripts/current_user_profile.js
@@ -0,0 +1,35 @@
+function showFavoriteProposals() {
+  hideRecentComments();
+  hideRecentVotes();
+  $("#favoriteProposals").css("display", "block");
+  $("#favProps").replaceWith(`<a class="detailsButton" id="favProps" href="#" onclick="hideFavoriteProposals(); return false;">Hide Proposals</a>`)
+}
+
+function hideFavoriteProposals() {
+  $("#favoriteProposals").css("display", "none");
+  $("#favProps").replaceWith(`<a class="detailsButton" id="favProps" href="#" onclick="showFavoriteProposals(); return false;">Favorite proposals</a>`)
+}
+
+function showRecentComments() {
+  hideFavoriteProposals();
+  hideRecentVotes();
+  $("#recentComments").css("display", "block");
+  $("#recComs").replaceWith(`<a class="detailsButton" id="recComs" href="#" onclick="hideRecentComments(); return false;">Hide Comments</a>`)
+}
+
+function hideRecentComments() {
+  $("#recentComments").css("display", "none");
+  $("#recComs").replaceWith(`<a class="detailsButton" id="recComs" href="#" onclick="showRecentComments(); return false;">Recent Comments</a>`)
+}
+
+function showRecentVotes() {
+  hideRecentComments();
+  hideFavoriteProposals();
+  $("#recentVotes").css("display", "block");
+  $("#recVotes").replaceWith(`<a class="detailsButton" id="recVotes" href="#" onclick="hideRecentVotes(); return false;">Hide Votes</a>`)
+}
+
+function hideRecentVotes() {
+  $("#recentVotes").css("display", "none");
+  $("#recVotes").replaceWith(`<a class="detailsButton" id="recVotes" href="#" onclick="showRecentVotes(); return false;">Recent Votes</a>`)
+}
