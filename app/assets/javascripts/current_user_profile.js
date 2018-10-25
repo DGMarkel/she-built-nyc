a2d35dd9e9f0ab35252743d0037292cdbfcedfc7
DGMarkel <davidgmarkel@gmail.com>
2018-10-24 22:44:45 -0400

    move functions to current_user_profile.js

--- app/assets/javascripts/current_user_profile.js
+++ app/assets/javascripts/current_user_profile.js
@@ -1,3 +1,37 @@
+function enlargeLoggedInIcon() {
+  $("#loggedInIcon").animate({
+    "width":"280px",
+    "height":"280px",
+    "border-bottom-right-radius":"25px",
+    "border-bottom-left-radius":"25px",
+    "border-top-right-radius":"0",
+    "border-top-left-radius":"0",
+    "top":"50px",
+    "right":"10.5%"},
+    1000)
+  $("#userDetails h1").animate({"margin-top":"290px"},1000,function(){})
+}
+
+function shrinkLoggedInIcon() {
+  $("a").on("click", function() {
+    animateShrinkage();
+  })
+}
+
+function animateShrinkage() {
+  $("#loggedInIcon").animate({
+    "top":"0",
+    "height":"40px",
+    "width":"40px",
+    "float":"right",
+    "margin-top":"10px",
+    "margin-right":"-40px",
+    "overflow": "hidden",
+    "border-radius":"50%"
+  },400,function(){});
+  $("#userDetails h1").animate({"margin-top":"0px"},1000,function(){})
+}
+
 function hideCurrentUserProfileTemplate() {
   $("#userDetails").animate({"right": "-36%"}, 500, function() {})
 }
