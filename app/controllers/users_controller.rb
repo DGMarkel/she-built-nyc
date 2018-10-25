ced5adde4a21d4d0840b3fe0b2c02257294b8c50
DGMarkel <davidgmarkel@gmail.com>
2018-10-24 22:02:24 -0400

    add new loginTemplate(), associated functions to log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :site_security, only: [:index, :edit, :update, :destroy]
+  before_action :site_security, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
